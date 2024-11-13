c0b92fabb3bd42020b2cf551e67bbb933ce60c39
KattanMohammad <mohammadsy107@gmail.com>
2024-11-13 16:57:12 +0100

    debug

--- pong2/Server.py
+++ pong2/Server.py
@@ -1,66 +1,72 @@
 import socket
-import signal #identifie les signaux pour kill le programme
-import sys #utilisé pour sortir du programme
+import sys
 import time
 import threading
-from ClientThread import ClientListener
 import random
+from ClientThread import ClientListener
+
 VITESSE_BALLE_X = 5
 VITESSE_BALLE_Y = 5
+
 class Server():
-    vitesse_balle_x = VITESSE_BALLE_X * random.choice([-1, 1])
-    vitesse_balle_y = VITESSE_BALLE_Y * random.choice([-1, 1])
-    pos_ball = "BALL"+" "+str(vitesse_balle_x) +" "+ str(vitesse_balle_y)+ " "
-    
     def __init__(self, port):
-        self.listener= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.vitesse_balle_x = VITESSE_BALLE_X * random.choice([-1, 1])
+        self.vitesse_balle_y = VITESSE_BALLE_Y * random.choice([-1, 1])
+        self.pos_ball = "BALL " + str(self.vitesse_balle_x) + " " + str(self.vitesse_balle_y) + " "
+        
+        self.listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.listener.bind(('', port))
         self.listener.listen(1)
         print("Listening on port", port)
-        self.clients_sockets= []
+        
+        self.clients_sockets = []
+        self.ball_thread_started = False  # Pour s'assurer qu'on ne crée qu'un seul thread pour la balle
 
     def run(self):
         while True:
-            print("listening new customers")
+            print("Listening for new clients...")
             try:
-                (client_socket, client_adress) = self.listener.accept()
+                (client_socket, client_address) = self.listener.accept()
             except socket.error:
                 sys.exit("Cannot connect clients")
+                
             self.clients_sockets.append(client_socket)
-            print("Start the thread for client:", client_adress)
-            client_thread= ClientListener(self, client_socket, client_adress)
-            client_thread.start()
-            time.sleep(0.1)
-            if len(self.clients_sockets)>1 : 
-                print("test from ball thread")
-                ball_thread = threading.Thread(target=self.sendBallPosition)
-                ball_thread.start()
+            print("Start the thread for client:", client_address)
             
+            
+            
+            if len(self.clients_sockets) > 1 and not self.ball_thread_started:
+                print("Starting the ball movement thread")
+                ball_thread = threading.Thread(target=self.sendBallPosition, daemon=True)
+                ball_thread.start()
+                client_thread = ClientListener(self, client_socket, client_address)
+                client_thread.start()
+                time.sleep(0.1)
+                self.ball_thread_started = True
+                
 
     def remove_socket(self, socket):
-        self.client_sockets.remove(socket)
-
+        if socket in self.clients_sockets:
+            self.clients_sockets.remove(socket)
 
     def sendBallPosition(self):
         while True:
+            self.pos_ball = "BALL " + str(self.vitesse_balle_x) + " " + str(self.vitesse_balle_y) + " "
             for sock in self.clients_sockets:
                 try:
                     sock.sendall(self.pos_ball.encode("UTF-8"))
                 except socket.error:
                     print("Cannot send the message")
-        
+            time.sleep(200)  # Ajout d'une pause pour éviter la surcharge et permettre les autres actions
+
     def echo(self, data):
-        print("echoing:", data)
-        print(self.pos_ball)
+        print("Echoing:", data)
         for sock in self.clients_sockets:
             try:
                 sock.sendall(data.encode("UTF-8"))
             except socket.error:
                 print("Cannot send the message")
 
-
-
-
 if __name__ == "__main__":
-    server= Server(59001)
+    server = Server(59001)
     server.run()
